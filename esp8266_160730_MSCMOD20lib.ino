d2e7636a911ee49ba6e5ac3557fe96d2025e56de
Yasuhiko OKADA <yasokada@gmail.com>
2016-07-30 13:43:30 +0900

    refactor

--- esp8266_160730_MSCMOD20lib.ino
+++ esp8266_160730_MSCMOD20lib.ino
@@ -36,7 +36,21 @@ bool readReply(int maxlen, char *dstPtr){
   return true;
 }
 
+bool receiveAck(char *dstPtr)
+{
+  int rcvlen = sizeof("!00");
+  char rcvstr[5] = { 0 }; // longer than "!00" + 1
+
+  bool rcvd = readReply(rcvlen, rcvstr);
+  if (rcvd && dstPtr != NULL) {
+    strncpy(dstPtr, rcvstr, rcvlen);
+  } 
+
+  return (strncmp(rcvstr, "!00", 3) == 0);
+}
+
 //--------------------------------------------------------------
+// public function
 
 void MSCMOD_ReadBootBanner(int rcvmaxlen, char *dstPtr)
 {
@@ -54,19 +68,6 @@ void MSCMOD_ReadBootBanner(int rcvmaxlen, char *dstPtr)
       break;
     }
   }
-}
-
-bool receiveAck(char *dstPtr)
-{
-  int rcvlen = sizeof("!00");
-  char rcvstr[5] = { 0 }; // longer than "!00" + 1
-
-  bool rcvd = readReply(rcvlen, rcvstr);
-  if (rcvd && dstPtr != NULL) {
-    strncpy(dstPtr, rcvstr, rcvlen);
-  } 
-
-  return (strncmp(rcvstr, "!00", 3) == 0);
 }
 
 bool MSCMOD_CheckWithAck(char *dstPtr)
